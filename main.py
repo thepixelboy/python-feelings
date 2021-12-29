c5eb786ed344dd602eea078028e0a81d45d8288f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-12-29 17:40:59 +0100

    feat: process user login credentials

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
 
+    def login(self, username, password):
+        with open("users.json") as file:
+            users = json.load(file)
+
+        if (username in users) and (users[username]["password"] == password):
+            self.manager.current = "login_screen_success"
+        else:
+            self.ids.login_failed.text = (
+                "Wrong username or password. Please, try again."
+            )
+
+
+class LoginScreenSuccess(Screen):
+    def logout(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
 
 class SignUpScreen(Screen):
     def add_user(self, username, password):
